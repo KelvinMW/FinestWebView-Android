9829bffae5fecaa61865d13bfce9699a9805605d
KelvinMW <mainakelvin360@gmail.com>
2021-06-30 19:15:39 +0300

    A default theme color

--- sample/src/main/java/com/thefinestartist/finestwebview/sample/MainActivity.java
+++ sample/src/main/java/com/thefinestartist/finestwebview/sample/MainActivity.java
@@ -16,7 +16,26 @@ public class MainActivity extends AppCompatActivity {
 
   public void onClick(View view) {
     if (view.getId() == R.id.ELMSecondary) {
-      new FinestWebView(this).titleDefault("ELM SECONDARY SCHOOL").show("https://secondary.elmischools.com");
+      new FinestWebView(this).titleDefault("ELM SECONDARY SCHOOL")
+      .showUrl(false)
+      .statusBarColorRes(R.color.bluePrimaryDark)
+      .toolbarColorRes(R.color.bluePrimary)
+      .titleColorRes(R.color.finestWhite)
+      .urlColorRes(R.color.bluePrimaryLight)
+      .iconDefaultColorRes(R.color.finestWhite)
+      .progressBarColorRes(R.color.finestWhite)
+      .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+      .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+      .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+      .showSwipeRefreshLayout(true)
+      .swipeRefreshColorRes(R.color.bluePrimaryDark)
+      .menuSelector(R.drawable.selector_light_theme)
+      .menuTextGravity(Gravity.CENTER)
+      .menuTextPaddingRightRes(R.dimen.defaultMenuTextPaddingLeft)
+      .dividerHeight(0)
+      .gradientDivider(false)
+      .setCustomAnimations(R.anim.slide_up, R.anim.hold, R.anim.hold, R.anim.slide_down)
+      .show("https://secondary.elmischools.com");
       ////                    .toolbarScrollFlags(0)
       //                    .webViewJavaScriptEnabled(true)
       //                    .webViewUseWideViewPort(false)
@@ -28,19 +47,24 @@ public class MainActivity extends AppCompatActivity {
       new FinestWebView(this)
           .theme(R.style.ELMscienceSecondary)
           .titleDefault("ELM SCIENCE SECONDARY SCHOOL")
-          .webViewBuiltInZoomControls(true)
-          .webViewDisplayZoomControls(true)
+          .showUrl(false)
+          .statusBarColorRes(R.color.bluePrimaryDark)
+          .toolbarColorRes(R.color.bluePrimary)
+          .titleColorRes(R.color.finestWhite)
+          .urlColorRes(R.color.bluePrimaryLight)
+          .iconDefaultColorRes(R.color.finestWhite)
+          .progressBarColorRes(R.color.finestWhite)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .showSwipeRefreshLayout(true)
+          .swipeRefreshColorRes(R.color.bluePrimaryDark)
+          .menuSelector(R.drawable.selector_light_theme)
+          .menuTextGravity(Gravity.CENTER)
+          .menuTextPaddingRightRes(R.dimen.defaultMenuTextPaddingLeft)
           .dividerHeight(0)
           .gradientDivider(false)
-          .setCustomAnimations(
-              R.anim.activity_open_enter,
-              R.anim.activity_open_exit,
-              R.anim.activity_close_enter,
-              R.anim.activity_close_exit)
-          .injectJavaScript(
-              "javascript: document.getElementById('msg').innerHTML='Hello "
-                  + "TheFinestArtist"
-                  + "!';")
+          .setCustomAnimations(R.anim.slide_up, R.anim.hold, R.anim.hold, R.anim.slide_down)
           .show("https://science-secondary.elmischools.com");
     } else if (view.getId() == R.id.ELMupper) {
       new FinestWebView(this)
@@ -69,31 +93,24 @@ public class MainActivity extends AppCompatActivity {
       new FinestWebView(this)
           .theme(R.style.FinestWebViewTheme)
           .titleDefault("ELM LOWER PRIMARY SCHOOL")
-          .toolbarScrollFlags(0)
-          .statusBarColorRes(R.color.blackPrimaryDark)
-          .toolbarColorRes(R.color.blackPrimary)
+          .showUrl(false)
+          .statusBarColorRes(R.color.bluePrimaryDark)
+          .toolbarColorRes(R.color.bluePrimary)
           .titleColorRes(R.color.finestWhite)
-          .urlColorRes(R.color.blackPrimaryLight)
+          .urlColorRes(R.color.bluePrimaryLight)
           .iconDefaultColorRes(R.color.finestWhite)
           .progressBarColorRes(R.color.finestWhite)
-          .swipeRefreshColorRes(R.color.blackPrimaryDark)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+          .showSwipeRefreshLayout(true)
+          .swipeRefreshColorRes(R.color.bluePrimaryDark)
           .menuSelector(R.drawable.selector_light_theme)
-          .menuTextGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT)
+          .menuTextGravity(Gravity.CENTER)
           .menuTextPaddingRightRes(R.dimen.defaultMenuTextPaddingLeft)
           .dividerHeight(0)
           .gradientDivider(false)
           .setCustomAnimations(R.anim.slide_up, R.anim.hold, R.anim.hold, R.anim.slide_down)
-          .setCustomAnimations(
-              R.anim.slide_left_in, R.anim.hold, R.anim.hold, R.anim.slide_right_out)
-          .setCustomAnimations(
-              R.anim.fade_in_fast,
-              R.anim.fade_out_medium,
-              R.anim.fade_in_medium,
-              R.anim.fade_out_fast)
-          .disableIconBack(true)
-          .disableIconClose(true)
-          .disableIconForward(true)
-          .disableIconMenu(true)
           .show("https://lower-p.elmischools.com");
         } else if (view.getId() == R.id.ELMkg) {
           new FinestWebView(this)
